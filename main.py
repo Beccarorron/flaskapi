5c8c4c8f9a15ddbcf8a79d9ce71449c4751408a9
Justin Hillis <justinhil@gmail.com>
2023-11-20 16:07:27 -0800

    test flask

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "HelloWorld"}
+        
+    def post(self):
+        return {"data": "Post"}
+
+
+api.add_resource(HelloWorld, "/helloworld")   
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
